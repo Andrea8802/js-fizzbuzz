3dc0963ce3d71e372aecfea3827d5fffd723115b
Andrea8802 <andreabonaventura802@gmail.com>
2022-11-09 14:25:41 +0100

    Structure Created

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// FizzBuzz che stampa a schermo dei numeri da 1 a 100, ma i multipli di 3 stampano "Fizz" e i multipli di 5 stampano "Buzz", se sono sia divisibili per 3 che per 5 stampano "FizzBuzz" 
+
+// Ciclo for per creare i numeri
+// Condizione che stabilisce i multipli di 3
+// Condizione che stabilisce i multipli di 5
+// Output in base ai multipli
